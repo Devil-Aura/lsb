b4a08a2dda741a9ad1066df4f9dfff270272d527
Devil-Aura <imh203356@gmail.com>
2026-02-01 13:41:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,12 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+# Credentials from your description
+API_ID = int(os.environ.get("API_ID", "22768311"))
+API_HASH = os.environ.get("API_HASH", "702d8884f48b42e865425391432b3794")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # Enter your Bot Token here or in ENV
+OWNER_ID = int(os.environ.get("OWNER_ID", "6040503076"))
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://link:link0123@cluster0.wm6zb7x.mongodb.net/?appName=Cluster0")
+DATABASE_NAME = "CrunchyRollVault"
 
-class Config:
-    API_ID = int(os.environ.get("APP_ID", "0"))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "linkssharing")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))  # For logs if needed
-    
-    # Customization Defaults
-    START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/f3d3aff9ec422158feb05-d2180e3665e0ac4d32.jpg")
-    
-    # Admins
-    ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()] if os.environ.get("ADMINS") else []
-    if OWNER_ID not in ADMINS:
-        ADMINS.append(OWNER_ID)
-
+# Web Port
+PORT = int(os.environ.get("PORT", 8080))
